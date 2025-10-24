016b9fa99677bd0dce80aeccaed7a3149f23784b
Austin Lee <austinlee@usf.edu>
2025-10-24 14:14:16 -0400

    button names

--- src/app/counter.jsx
+++ src/app/counter.jsx
@@ -1,15 +1,15 @@
 'use client';
 import {useState} from 'react';
 export default function Counter () {
-    const [ count, setCount]= useState(0);
+    const [count, setCount]= useState(0);
 
     return( 
         <div>
             <p>Current Sales Count: {count}</p>
-            <button id="add" onClick={() => setCount(count +1)}></button>
+            <button id="add" onClick={() => setCount(count +1)}>Add One</button>
             
-            <button id="subtract" onClick={() => setCount(count -1)}></button>
-            <button id="reset" onClick={() => setCount(0)}></button>
+            <button id="subtract" onClick={() => setCount(count -1)}>Subtract One</button>
+            <button id="reset" onClick={() => setCount(0)}>Reset</button>
 
         </div>
     )
