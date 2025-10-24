a7de8695beb3713c6a17ceeac6ae079127dcbf07
Austin Lee <austinlee@usf.edu>
2025-10-24 14:10:58 -0400

    camalCase

--- src/app/counter.jsx
+++ src/app/counter.jsx
@@ -6,10 +6,10 @@ export default function Counter () {
     return( 
         <div>
             <p>Current Sales Count: {count}</p>
-            <button id="add" onclick={() => setCount(count +1)}></button>
+            <button id="add" onClick={() => setCount(count +1)}></button>
             
-            <button id="subtract" onclick={() => setCount(count -1)}></button>
-            <button id="reset" onclick={() => setCount(0)}></button>
+            <button id="subtract" onClick={() => setCount(count -1)}></button>
+            <button id="reset" onClick={() => setCount(0)}></button>
 
         </div>
     )
