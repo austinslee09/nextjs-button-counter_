a01c5738bdffea969f942dfee59c40683d62aa39
Austin Lee <austinlee@usf.edu>
2025-10-24 14:09:56 -0400

    counter capitalized

--- src/app/counter.jsx
+++ src/app/counter.jsx
@@ -1,13 +1,13 @@
 'use client';
 import {useState} from 'react';
 export default function Counter () {
-    const [ count, setCount]= useState(0)
+    const [ count, setCount]= useState(0);
 
     return( 
         <div>
             <p>Current Sales Count: {count}</p>
             <button id="add" onclick={() => setCount(count +1)}></button>
-            <br />
+            
             <button id="subtract" onclick={() => setCount(count -1)}></button>
             <button id="reset" onclick={() => setCount(0)}></button>
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home (){
   return(
     <main>
       <center> Counter Section
-        <counter />
+        <Counter />
       </center>
     </main>
   )
