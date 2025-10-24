e8f6550282f36f22fb9a54e72851763c526ae804
Austin Lee <austinlee@usf.edu>
2025-10-24 14:04:39 -0400

    set count 0

--- src/app/counter.jsx
+++ src/app/counter.jsx
@@ -8,7 +8,7 @@ export default function Counter () {
             <p>Current Sales Count: {count}</p>
             <button id="add" onclick={() => setCount(count +1)}></button>
             <button id="subtract" onclick={() => setCount(count -1)}></button>
-            <button id="reset" onclick={() => setCount(count 0)}></button>
+            <button id="reset" onclick={() => setCount(0)}></button>
 
         </div>
     )
